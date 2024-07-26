74f31f0d9cc0bce09d1639fa9ee581d9058050ab
Reno Davids <reno.davids@sovtech.com>
2024-07-26 16:00:18 +0200

    added bad code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+// src/App.tsx
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState<any>([]);
+    const [newTodo, setNewTodo] = useState('');
+
+    const addTodo = () => {
+        setTodos([...todos, { text: newTodo, completed: false }]);
+        setNewTodo('');
+    };
+
+    const toggleTodo = (index: number) => {
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed;
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="App">
+            <h1>Todo List</h1>
+            <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+            <button onClick={addTodo}>Add Todo</button>
+            <ul>
+                {todos.map((todo: any, index: number) => (
+                    <li key={index} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+                        <span onClick={() => toggleTodo(index)}>{todo.text}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
